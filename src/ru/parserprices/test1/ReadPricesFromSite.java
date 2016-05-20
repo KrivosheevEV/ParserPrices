fe77b3aff02ba4eb6af9771fc986b424553f17e8
KrivosheevEV <krivosheevev163rus@gmail.com>
2016-05-20 09:57:09 -0400

    1

---
+++ src/ru/parserprices/test1/ReadPricesFromSite.java
@@ -0,0 +1,18 @@
+package ru.parserprices.test1;
+
+
+public class ReadPricesFromSite {
+
+    public static void main(String[] args) {
+
+
+        int mCount = 10;
+        int[] mArray = new int[1];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i++);
+        }
+
+
+    }
+}
