ac5cd9eabfac075d5a84f94a138eec94adc3ad1c
KrivosheevEV <euja@yandex.ru>
2016-05-25 18:21:52 +0400

    11

--- src/ru/parserprices/myparser/ReadPricesFromSite.java
+++ src/ru/parserprices/myparser/ReadPricesFromSite.java
@@ -2,6 +2,8 @@ package ru.parserprices.myparser;
 
 
 import org.jsoup.Jsoup;
+import org.jsoup.examples.HtmlToPlainText;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
@@ -10,23 +12,62 @@ import java.io.IOException;
 public class ReadPricesFromSite {
 
     private static final String userAgent = "Mozilla/5.0 (jsoup)";
-    private static final int timeout = 5 * 1000 ;
+    private static final int timeout = 7 * 1000 ;
 
     public static void main(String[] args) throws IOException  {
 
+        Elements elements1, elements2, elements3, elements4;
+        String stringToLog = "";
+//        String url = "http://dns-shop.ru";
+        String url = "http://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/";
+        String selector1 = "product";
+        String selector2 = "item-name";
+        String selector3 = "ec-price-item-link";
+        String selector4 = "prev-price-total";
 
-        Elements links = null;
-        try {
-            links = Jsoup.connect("http://dns-shop.ru").get().getElementsByTag("a");
-        } catch (IOException e) {
-            e.printStackTrace();
+        Document doc = Jsoup.connect(url).userAgent(userAgent).timeout(timeout).get();
+
+        HtmlToPlainText formatter = new HtmlToPlainText();
+
+//        if (selector != null) {
+            elements1 = doc.getElementsByAttributeValue("class", selector1);
+
+        for (Element element1 : elements1) {
+
+
+            elements2 = element1.getElementsByAttributeValue("class", selector2);
+
+//            for (Element element2 : elements2) {
+
+                System.out.print(elements2.get(0).text() + ", ");
+                elements3 = element1.getElementsByAttributeValue("class", selector3);
+
+//                for (Element element3 : elements3) {
+
+                   System.out.print(elements3.get(0).text() + ", ");
+                    elements4 = element1.getElementsByAttributeValue("class", selector4);
+//                    for (Element element4 : elements4) {
+                        System.out.print(elements4.get(0).text() + ". \n");
+//                        stringToLog.concat(formatter.getPlainText(element4) + ", ");
+//                        System.out.print(formatter.getPlainText(element4));
+//                    }
+//                }
+//            }
+
+//            System.out.print(stringToLog);
         }
 
-        for (Element link : links) {
-            System.out.println(link.text());
-
-        }
 
 
+
+//                String plainText = formatter.getPlainText(element); // format that element to plain text
+//                System.out.println(plainText);
+//        } else { // format the whole doc
+//            String plainText = formatter.getPlainText(doc);
+//            System.out.println(plainText);
+//        }
+
+
+//        System.out.print(elements4.size());
     }
 }
--- src/ru/parserprices/myparser/ReadSite.java
+++ src/ru/parserprices/myparser/ReadSite.java
@@ -4,4 +4,23 @@ package ru.parserprices.myparser;
  * Created by vnc on 5/21/16.
  */
 public class ReadSite {
+
+//    WebDriver driver = new FirefoxDriver();
+//    driver.get("http://www.somesite.com);
+//
+//            if(driver.findElement(By.name("username")) != null){
+//        WebElement element_name = driver.findElement(By.name("username"));
+//        WebElement element_password = driver.findElement(By.name("password"));
+//        element_name.sendKeys("youruname");
+//        element_password.sendKeys("yourpass");
+//        WebElement element_submit = driver.findElement(By.xpath("//input[@type='submit']"));
+//        element_submit.click();
+//        String curentURL = driver.getCurrentUrl();
+//        System.out.println(curentURL);
+//    } else {
+//        WebElement element_submit = driver.findElement(By.xpath("//input[@type='submit']"));
+//        element_submit.click();
+//    }
+//    WebElement element_submit = driver.findElement(By.xpath("//input[@type='submit']"));
+//    element_submit.click();
 }
