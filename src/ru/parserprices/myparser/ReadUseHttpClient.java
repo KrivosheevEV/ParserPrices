2383c1f9a51db0f1a96b55665b1ccf562b68f337
KrivosheevEV <euja@yandex.ru>
2016-05-27 13:33:21 +0400

    11

--- src/ru/parserprices/myparser/ReadUseHttpClient.java
+++ src/ru/parserprices/myparser/ReadUseHttpClient.java
@@ -1,11 +1,13 @@
 package ru.parserprices.myparser;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.SearchContext;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.*;
 
@@ -15,7 +17,8 @@ public class ReadUseHttpClient {
 
     public void ReadSite() throws InterruptedException {
 
-        driver1 = new HtmlUnitDriver();
+//        driver1 = new HtmlUnitDriver();
+        driver1 = new FirefoxDriver();
 
         String siteAdress = "http://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/";
         String nextPage = "http://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/#";
@@ -50,20 +53,44 @@ public class ReadUseHttpClient {
 //        System.out.print(element1.getText() + "\n");
 //        element1.click();
 
+        List<WebElement> listElements2 = driver1.findElements(By.className("thumbnail"));
+        System.out.print("\n" + listElements2.size() + "\n");
+
         List<WebElement> listElements1 = driver1.findElements(By.className("catalog-category-more"));
 
         for (WebElement element1 : listElements1) {
 
             WebElement element2 = element1.findElement(By.className("btn-default"));
-            System.out.print(element2.getText());
+//            WebElement element2 = element1.findElement(By.className("glyphicon-triangle-bottom"));
+            System.out.print(element2.getText() + "\n");
             element2.click();
+
+//            Actions actions = new Actions(driver1);
+//            actions.moveToElement(element2, 1, 1).click();
+
+//            JavascriptExecutor executor = (JavascriptExecutor) driver1;
+//            executor.executeScript("arguments[0].click();", element2);
+        }
+
+//        Thread.sleep(5000);
+
+        int maxWaitTime = 5;
+        String className = "products-list-continue";
+        try {
+            (new WebDriverWait(driver1, maxWaitTime))
+                    .until(ExpectedConditions
+                            .visibilityOfElementLocated(By.className(className)));
+
+        } catch (Throwable te) {
+            System.out.print("Unable to find the element by classname: '"
+                    + className + "' within " + maxWaitTime + " seconds.");
+            //throw new TimeoutException(te);
         }
 
 
-        List<WebElement> listElements2 = driver1.findElements(By.className("product"));
+
+        listElements2 = driver1.findElements(By.className("thumbnail"));
         System.out.print("\n" + listElements2.size());
-
-
 
 //        System.out.println("\n Page title is: " + driver.getCurrentUrl());
 
