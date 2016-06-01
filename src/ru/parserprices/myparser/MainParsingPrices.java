0cd1e131075812fa212ef8c695e3bbffe3405e2e
KrivosheevEV <euja@yandex.ru>
2016-06-01 15:46:28 +0400

    11

--- src/ru/parserprices/myparser/MainParsingPrices.java
+++ src/ru/parserprices/myparser/MainParsingPrices.java
@@ -1,10 +1,9 @@
 package ru.parserprices.myparser;
 
 
-import java.io.File;
 import java.io.IOException;
 
-public class ReadPricesFromSite {
+public class MainParsingPrices {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
@@ -12,14 +11,14 @@ public class ReadPricesFromSite {
 //        mPatternOfSite.setFullAddress("D:/Temp/", "Pattern_DNS1.txt");
         mPatternOfSite.setFullAddress(mPatternOfSite.getCurrentPath(), "Pattern_DNS1.xml");
 
-        ReadSiteUseFirefoxDriver readSiteUseFirefoxDriver = new ReadSiteUseFirefoxDriver();
-        readSiteUseFirefoxDriver.ReadSite(mPatternOfSite.getFullAddress());
+        ReadSiteDNS readSite_DNS = new ReadSiteDNS();
+        readSite_DNS.ReadSite(mPatternOfSite.getFullAddress());
 
         ReadWriteFile mResultOfParsing = new ReadWriteFile();
         mResultOfParsing.setFullAddress(mPatternOfSite.getCurrentPath(), "/Result_DNS1.txt");
 //
-        mResultOfParsing.writeResultToFile(mResultOfParsing.getFullAddress(), readSiteUseFirefoxDriver.getResultOfParsing());
-//        mResultOfParsing.writeResultToFile("D:/Projects/ParserPrices/out/artifacts/ParserPrices_jar/1.txt", readSiteUseFirefoxDriver.getResultOfParsing());
+        mResultOfParsing.writeResultToFile(mResultOfParsing.getFullAddress(), readSite_DNS.getResultOfParsing());
+//        mResultOfParsing.writeResultToFile("D:/Projects/ParserPrices/out/artifacts/ParserPrices_jar/1.txt", readSite_DNS.getResultOfParsing());
 
 
     }
--- src/ru/parserprices/myparser/ReadSiteDNS.java
+++ src/ru/parserprices/myparser/ReadSiteDNS.java
@@ -14,10 +14,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-public class ReadSiteUseFirefoxDriver {
+public class ReadSiteDNS {
 
     private String resultOfPasring = "";
-    long startTime;
+    private long startTime;
+
+    private static String DNG_GENERAL_URL = "http://www.dns-shop.ru";
+    private static int WAITING_FOR_EXPAND = 10;
 
     public void ReadSite(String FullAddress) throws InterruptedException {
 
@@ -26,26 +29,49 @@ public class ReadSiteUseFirefoxDriver {
 
 //        WebDriver driver = new HtmlUnitDriver();
 //        WebDriver driver = new FirefoxDriver();
+
         FirefoxProfile profile = new FirefoxProfile();
         WebDriver driver = new FirefoxDriver(profile);
 
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
 
 //        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        String siteAdress = "http://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/";
-
         String cssSelector_ExpandPage_Block = "div.catalog-category-more, div[style$='block']";
         String cssSelector_ExpandPage_Button = "a[class='btn btn-default']";
+        String cssSelector_Categories = "div.menu-item-category-wrapper a[href]";
         String cssSelector1 = "div.thumbnail";
 
-        driver.get(siteAdress);
 
-        ExpandAllPages(driver, cssSelector_ExpandPage_Block, cssSelector_ExpandPage_Button, cssSelector1);
 
-        System.out.print(resultOfPasring);
+        ArrayList<String> listPages = new ArrayList<String>();
+        listPages.add("http://www.dns-shop.ru/catalog/17a892f816404e77/noutbuki/");
+        //listPages.add("http://www.dns-shop.ru/catalog/31f05737df7e4e77/ssd-25-sata-nakopiteli/");
+
+        fillListPages(driver, cssSelector_Categories, listPages);
+
+        for (int countSites = 0; countSites < listPages.size(); countSites++) {
+
+            addToResultString(Integer.toString(countSites + 1));
+
+            // Open browser.
+            addToResultString("Open page: " + listPages.get(countSites));
+            driver.navigate().to(listPages.get(countSites));
+//            addToResultString("Finish open browser");
+
+            // Expand all pages with goods.
+            addToResultString("Start expand pages");
+            expandAllPages(driver, cssSelector_ExpandPage_Block, cssSelector_ExpandPage_Button, cssSelector1);
+//            addToResultString("Finish expand pages");
+        }
+
+        // Close browser.
         driver.close();
         driver.quit();
+
+//        System.out.println(listPages.size());
+        System.out.print(resultOfPasring);
+
     }
 
     // Disable warning in console.
@@ -54,7 +80,7 @@ public class ReadSiteUseFirefoxDriver {
     }
     ///
 
-    public void ExpandAllPages(WebDriver driver, String cssSelector_ExpandPage_Block, String cssSelector_ExpandPage_Button, String cssSelector1) {
+    public void expandAllPages(WebDriver driver, String cssSelector_ExpandPage_Block, String cssSelector_ExpandPage_Button, String cssSelector1) {
 
         try {
 
@@ -63,7 +89,7 @@ public class ReadSiteUseFirefoxDriver {
             if (listItems.size() > 0) {
 
                 WebElement but_NextPageBlock = driver.findElement(By.cssSelector(cssSelector_ExpandPage_Block));
-                int countOfClick = 1;
+//                int countOfClick = 1;
 
                 while (but_NextPageBlock != null) {
 
@@ -73,14 +99,15 @@ public class ReadSiteUseFirefoxDriver {
                     WebElement but_NextPageButton = but_NextPageBlock.findElement(By.cssSelector(cssSelector_ExpandPage_Button));
 
                     try {
-                        while ((new WebDriverWait(driver, 30)).until(ExpectedConditions.not(
+                        while ((new WebDriverWait(driver, WAITING_FOR_EXPAND)).until(ExpectedConditions.not(
                                 ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(cssSelector_ExpandPage_Block))))) {
 
                             but_NextPageButton.sendKeys(Keys.ESCAPE);
                             but_NextPageButton.click();
 
-                            System.out.print(countOfClick + but_NextPageButton.getText() + "\n");
-                            addToResultString(Integer.toString(countOfClick++));
+                            //System.out.print(countOfClick + but_NextPageButton.getText() + "\n");
+                            //addToResultString(Integer.toString());
+//                            countOfClick++;
                         }
 
                     } catch (TimeoutException te) {
@@ -90,7 +117,7 @@ public class ReadSiteUseFirefoxDriver {
                         //addToResultString(te.getMessage());
 
                     } finally {
-                        addToResultString("Finished expand pages");
+                        //addToResultString("Finished expand pages");
                     }
                 }
 
@@ -99,10 +126,30 @@ public class ReadSiteUseFirefoxDriver {
             }
 
         } catch (Throwable te) {
-            addToResultString("Error parsing sites.");
+            addToResultString("Error parsing site (expand)");
             addToResultString(te.getMessage());
         }
+    }
 
+    private void fillListPages(WebDriver driver, String cssSelector_Categories,  ArrayList<String> listPages) {
+
+        driver.navigate().to(DNG_GENERAL_URL);
+
+        try {
+
+            List<WebElement> listItems = driver.findElements(By.cssSelector(cssSelector_Categories));
+
+            for (WebElement hrefElement : listItems
+                 ) {
+                String linkCategory = hrefElement.getAttribute("href");
+                listPages.add(linkCategory);
+                //addToResultString(linkCategory);
+            }
+
+        } catch (Throwable te) {
+            addToResultString("Error parsing site (fill category).");
+            addToResultString(te.getMessage());
+        }
     }
 
     public String getResultOfParsing(){
@@ -111,7 +158,9 @@ public class ReadSiteUseFirefoxDriver {
     }
 
     private void addToResultString(String addedString){
-        resultOfPasring = resultOfPasring.concat(Long.toString(System.currentTimeMillis() - startTime) + " -> " + addedString + "\n");
+        //if (addedString.isEmpty()) return;
+        String timeForResult = Long.toString((System.currentTimeMillis() - startTime) / 1000) + "." + Long.toString((System.currentTimeMillis() - startTime) % 1000);
+        resultOfPasring = resultOfPasring.concat(timeForResult + " -> " + addedString + "\n");
     }
 
     private boolean elementCanFind(WebDriver givenDriver, By givenBy){
